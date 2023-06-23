e89d16479260b05b95c6fa4bddbcede29aa6ed11
Sidh Jain <sidhj211@gmail.com>
2023-06-24 00:15:21 +0530

    Added functionality to the user button
    Now when customer button is pressed , user will be rerouted to the /menu page

--- next/core/src/app/page.js
+++ next/core/src/app/page.js
@@ -28,7 +28,7 @@ export default function Page() {
     <body className='bg-rose-10'>
       <h1 className='text-4xl font-serif text-center text-white p-4 bg-rose-400'>Login</h1>
     <div className='flex justify-evenly items-center pt-24'>
-      <button className='m-4 border-none border-2 rounded-md shadow-xl bg-teal-500 text-center text-white p-6 cursor-pointer font-bold font-sans hover:shadow-teal-800/40'><img src='/Customer Icon.jpg' width={150} className='rounded-xl p-1'/><hr />Customer</button>
+      <button  onClick={() => router.push('/menu')}className='m-4 border-none border-2 rounded-md shadow-xl bg-teal-500 text-center text-white p-6 cursor-pointer font-bold font-sans hover:shadow-teal-800/40'><img src='/Customer Icon.jpg' width={150} className='rounded-xl p-1'/><hr />Customer</button>
       <button  onClick={() => router.push('/kitchen')} className='m-4 border-none border-2 rounded-md shadow-xl bg-teal-500 text-center text-white p-6 cursor-pointer font-bold font-sans hover:shadow-teal-800/40'><img src='/Kitchen Icon.jpg' width={150} className='rounded-xl p-1'/><hr />Kitchen</button>
       <button className='m-4 border-none border-2 rounded-md shadow-xl bg-teal-500 text-center text-white p-6 cursor-pointer font-bold font-sans hover:shadow-teal-800/40'><img src='/Cash Counter Icon.jpg' width={150} className='rounded-xl p-1'/><hr />Cash Counter</button>
     </div>
