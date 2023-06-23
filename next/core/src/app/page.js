eed58189647d76c9f00e37bd40a69f6ed9ff9b6a
Sidh Jain <sidhj211@gmail.com>
2023-06-23 23:29:52 +0530

    Gave functionality to kitchen button on home page, made kitchen css
    the localhost/3000/kitchen is hardcoded....
    changed the const Login=() => ( it is a part of react component i dunno if it will affect the code later) with export default function

--- next/core/src/app/page.js
+++ next/core/src/app/page.js
@@ -1,6 +1,7 @@
+'use client'
+ import Link from 'next/link';
+ import { useRouter } from 'next/navigation';
 
-// 'use client'
-// import Link from 'next/link';
 // import React, { useRef } from 'react'
 // import { BiFoodMenu } from 'react-icons/bi';
 // import { AiOutlinePlusSquare, AiOutlineMinusSquare, AiOutlineCloseCircle } from 'react-icons/ai';
@@ -21,18 +22,20 @@
 
 import React from 'react'
 
-const Login = () => {
+export default function Page() {
+  const router = useRouter()
   return (
-    <body className='bg-rose-100'>
+    <body className='bg-rose-10'>
       <h1 className='text-4xl font-serif text-center text-white p-4 bg-rose-400'>Login</h1>
     <div className='flex justify-evenly items-center pt-24'>
       <button className='m-4 border-none border-2 rounded-md shadow-xl bg-teal-500 text-center text-white p-6 cursor-pointer font-bold font-sans hover:shadow-teal-800/40'><img src='/Customer Icon.jpg' width={150} className='rounded-xl p-1'/><hr />Customer</button>
-      <button className='m-4 border-none border-2 rounded-md shadow-xl bg-teal-500 text-center text-white p-6 cursor-pointer font-bold font-sans hover:shadow-teal-800/40'><img src='/Kitchen Icon.jpg' width={150} className='rounded-xl p-1'/><hr />Kitchen</button>
+      <button  onClick={() => router.push('/kitchen')} className='m-4 border-none border-2 rounded-md shadow-xl bg-teal-500 text-center text-white p-6 cursor-pointer font-bold font-sans hover:shadow-teal-800/40'><img src='/Kitchen Icon.jpg' width={150} className='rounded-xl p-1'/><hr />Kitchen</button>
       <button className='m-4 border-none border-2 rounded-md shadow-xl bg-teal-500 text-center text-white p-6 cursor-pointer font-bold font-sans hover:shadow-teal-800/40'><img src='/Cash Counter Icon.jpg' width={150} className='rounded-xl p-1'/><hr />Cash Counter</button>
     </div>
     </body>
   )
 }
 
-export default Login
+
+
 
